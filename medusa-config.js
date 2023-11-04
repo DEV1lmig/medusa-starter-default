42d82a196472267875f90909663806618c7f0f1c
DEV1lmig <maquiroz@urbe.edu.ve>
2023-11-04 12:52:01 -0400

    yeeep

--- medusa-config.js
+++ medusa-config.js
@@ -61,17 +61,7 @@ const plugins = [
       clientSecret: process.env.PAYPAL_CLIENT_SECRET,
       authWebhookId: process.env.PAYPAL_AUTH_WEBHOOK_ID,
     },
-  },
-  {
-    resolve: `medusa-file-s3`,
-    options: {
-        s3_url: process.env.S3_URL,
-        bucket: process.env.S3_BUCKET,
-        region: process.env.S3_REGION,
-        access_key_id: process.env.S3_ACCESS_KEY_ID,
-        secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
-    },
-  },
+  }
 ];
 
 const modules = {
